cc7f70b498bb5e346ec5462315f8ebd1e246392b
zachesk <55416371+zachesk@users.noreply.github.com>
2022-10-11 15:44:15 -0600

    Update CFAG240128U0.ino
    Correct pin table

--- CFAG240128U0/CFAG240128U0.ino
+++ CFAG240128U0/CFAG240128U0.ino
@@ -17,8 +17,8 @@
 //  #8/D8     |  PB0 | LCD_DC Data/Command or Register Select
 //  #9/D9     |  PB1 | LCD_RESET
 // #10/D10    |  PB2 | LCD_CS_NOT   (or SPI SS)
-// #11/D11    |  PB3 | LCD_MOSI/DB7 (hardware SPI)
-// #13/D13    |  PB5 | LCD_SCK/DB6  (hardware SPI)
+// #11/D11    |  PB3 | LCD_MOSI/DB1 (hardware SPI)
+// #13/D13    |  PB5 | LCD_SCK/DB7  (hardware SPI)
 //
 // You will also need to tie the interface pins (IF0 and IF1) low for SPI
 // see datasheet for other interfaces
